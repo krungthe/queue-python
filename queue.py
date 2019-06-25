94c5e4d03f2f3454e609535d3ffcbd119c8da64f
krungthe <48580027+krungthe@users.noreply.github.com>
2019-06-25 21:52:33 +0700

    working copy

--- queue.py
+++ queue.py
@@ -7,8 +7,9 @@ nextNumber = 1
 currentNumber = startNumber + 1
 while nextNumber != "0":
     nextNumber = input ("press enter when the next queue number is called; 0 to exit ")
+    end = time.perf_counter()
+    timeLeft = ((end - start) / (currentNumber - startNumber)) * (myNumber - currentNumber)
     currentNumber = currentNumber + 1
     print("current number:", currentNumber)
-    print("your turn is in x minutes")
-    end = time.perf_counter()
+    print("your turn is in %d seconds" % timeLeft)
     print (end - start)

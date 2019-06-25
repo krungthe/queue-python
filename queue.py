521bc0d672d5dc5f8957e32072fa54a3ecea3362
krungthe <48580027+krungthe@users.noreply.github.com>
2019-06-25 21:32:05 +0700

    timer added

--- queue.py
+++ queue.py
@@ -1,5 +1,7 @@
-import sys
+import time
+
 startNumber = int(input("Current number? "))
+start = time.perf_counter()
 myNumber = int(input ("your number? "))
 nextNumber = 1
 currentNumber = startNumber + 1
@@ -8,3 +10,5 @@ while nextNumber != "0":
     currentNumber = currentNumber + 1
     print("current number:", currentNumber)
     print("your turn is in x minutes")
+    end = time.perf_counter()
+    print (end - start)

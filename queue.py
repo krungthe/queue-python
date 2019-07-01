34a9246a7d80818e3e10abf57e3fbdc77ea86c23
krungthe <krungthep.kaki@gmail.com>
2019-07-01 14:04:49 +0700

    improvements

--- queue.py
+++ queue.py
@@ -6,20 +6,20 @@ myNumber = int(input ("your number? "))
 nextNumber = 1
 currentNumber = startNumber
 while nextNumber != "0":
-    nextNumber = input ("enter when the next number is called; 0 to exit\n")
+    nextNumber = input ("Enter when next number is called; 0 to exit\n")
     currentNumber = currentNumber + 1
     end = time.perf_counter()
     timeLeft = ((end - start) / (currentNumber - startNumber)) * (myNumber - currentNumber)
-    print("current number:", currentNumber)
+    print(f"current number: {currentNumber} (-{myNumber - currentNumber})")
     if currentNumber < myNumber :
         if timeLeft < 60 :
             print("ETA: %d seconds" % timeLeft)
         else :
             minutesLeft = int(timeLeft / 60)
             secondsLeft = int(timeLeft - (minutesLeft * 60))
-            timeLeftString = "{}m {}s"
-            print("ETA:" + timeLeftString.format(minutesLeft, secondsLeft))
-        print ("wait:", int(end - start), "s\n") 
+            #timeLeftString = "{}m {}s"
+            print(f"ETA: {minutesLeft}m {secondsLeft}s");
+        print (f"wait: {int(end - start)}s\n");
     else:
         print("Your turn! total wait:", int(end -start),"s");
         quit()

5dfeaff05f7012990a851a9b7576cb9926a2c5b4
krungthe <krungthep.kaki@gmail.com>
2019-07-17 10:22:32 +0700

    some improvements

--- queue.py
+++ queue.py
@@ -1,24 +1,53 @@
 import time
 
-startNumber = int(input("Current number? "))
+def timeFormat(secs):
+    if secs < 60:
+        string = str(secs) + "s"
+        return string
+    elif secs < 3600:
+        minutes = secs // 60
+        rest = secs % 60
+        string = str(minutes) + "m " + str(rest) + "s"
+        return string
+    else:
+        hours = secs // 3600
+        minutes = (secs - (hours * 3600)) // 60
+        rest = secs -(hours * 3600) - (minutes * 60)
+        string = str(hours) + "h " + str(minutes) + "m " + str(rest) + "s"
+        return string
+
+string = ""
+string2 = ""
+startNumber = ""
+myNumber = ""
+firstQuestion = "Current number? "
+secondQuestion = "Your number? "
+
+while startNumber.isdigit() == False:
+    startNumber = input(firstQuestion)
+    firstQuestion = "Current NUMBER? "
+
+startNumber = int(startNumber)
 start = time.perf_counter()
-myNumber = int(input ("your number? "))
+
+while myNumber.isdigit() == False:
+    myNumber = input(secondQuestion)
+    secondQuestion = "Your NUMBER? "
+
+myNumber = int(myNumber)
 nextNumber = 1
 currentNumber = startNumber
-while nextNumber != "0":
-    nextNumber = input ("Enter when next number is called; 0 to exit\n")
+while nextNumber != "q":
+    nextNumber = input ("Enter when next number is called; q to exit\n")
     currentNumber = currentNumber + 1
     end = time.perf_counter()
-    timeLeft = ((end - start) / (currentNumber - startNumber)) * (myNumber - currentNumber)
+    timeLeft = int(((end - start) / (currentNumber - startNumber)) * (myNumber - currentNumber))
     print(f"current number: {currentNumber} (-{myNumber - currentNumber})")
     if currentNumber < myNumber :
-        if timeLeft < 60 :
-            print("ETA: %d seconds" % timeLeft)
-        else :
-            minutesLeft = int(timeLeft / 60)
-            secondsLeft = int(timeLeft - (minutesLeft * 60))
-            print(f"ETA: {minutesLeft}m {secondsLeft}s")
-        print (f"wait: {int(end - start)}s\n")
+        string = timeFormat(timeLeft)
+        string2 = str(timeFormat((int(end - start))))
+        print("ETA: " + string )
+        print ("wait: " + string2)
     else:
-        print("Your turn! total wait:", int(end -start),"s")
+        print("Your turn! total wait:", str(timeFormat((int(end - start)))))
         quit()
---
+++ queue_old.py
@@ -0,0 +1,24 @@
+import time
+
+startNumber = int(input("Current number? "))
+start = time.perf_counter()
+myNumber = int(input ("your number? "))
+nextNumber = 1
+currentNumber = startNumber
+while nextNumber != "0":
+    nextNumber = input ("Enter when next number is called; 0 to exit\n")
+    currentNumber = currentNumber + 1
+    end = time.perf_counter()
+    timeLeft = ((end - start) / (currentNumber - startNumber)) * (myNumber - currentNumber)
+    print(f"current number: {currentNumber} (-{myNumber - currentNumber})")
+    if currentNumber < myNumber :
+        if timeLeft < 60 :
+            print("ETA: %d seconds" % timeLeft)
+        else :
+            minutesLeft = int(timeLeft / 60)
+            secondsLeft = int(timeLeft - (minutesLeft * 60))
+            print(f"ETA: {minutesLeft}m {secondsLeft}s")
+        print (f"wait: {int(end - start)}s\n")
+    else:
+        print("Your turn! total wait:", int(end -start),"s")
+        quit()
